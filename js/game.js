2b424aece521379c043d295f4332d0fef48c537c
JimmyYangsix <1075887609@qq.comm>
2018-01-25 16:22:14 +0800

    18.1.25

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var $body=$(document.body);
+//画布获取
+var $canvas=$('#game');
+var canvas= $canvas.get(0);
+var cantext= canvas.getContext('2d');
+//设置宽度与高度
+canvas.width=window.width;
+canvas.height=window.height;
+
+//进入函数
+function init() {
+    
+}
+init()
